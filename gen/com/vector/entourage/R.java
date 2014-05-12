ee8cb6b2ef061bd220198f488b2a7514aa48c00d
damion6891 <damion6891@yahoo.com>
2014-05-12 19:58:52 -0400

    Base-changes

--- gen/com/vector/entourage/R.java
+++ gen/com/vector/entourage/R.java
@@ -21,21 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int o_icon=0x7f020001;
-        public static final int vectorlogo=0x7f020002;
+        public static final int ent_logo_phone=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int o_icon=0x7f020002;
     }
     public static final class id {
-        public static final int A=0x7f080006;
-        public static final int E=0x7f080008;
-        public static final int G=0x7f080007;
-        public static final int N=0x7f080001;
-        public static final int R=0x7f080005;
-        public static final int T=0x7f080002;
-        public static final int U=0x7f080004;
-        public static final int action_settings=0x7f080009;
-        public static final int numericE=0x7f080000;
-        public static final int o_icon=0x7f080003;
+        public static final int A=0x7f080007;
+        public static final int E=0x7f080009;
+        public static final int G=0x7f080008;
+        public static final int N=0x7f080002;
+        public static final int R=0x7f080006;
+        public static final int T=0x7f080003;
+        public static final int U=0x7f080005;
+        public static final int action_settings=0x7f08000e;
+        public static final int container=0x7f080000;
+        public static final int login=0x7f08000b;
+        public static final int login_label=0x7f08000a;
+        public static final int numericE=0x7f080001;
+        public static final int o_icon=0x7f080004;
+        public static final int password=0x7f08000c;
+        public static final int password_label=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -56,9 +61,14 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int icon_content_description=0x7f05000b;
-        /**   Login title animation  
+        /**   Login View Strings  
+         */
+        public static final int login_label=0x7f05000c;
+        public static final int login_password=0x7f05000e;
+        /**   Login title animation.  
          */
         public static final int numericE=0x7f050003;
+        public static final int password_label=0x7f05000d;
     }
     public static final class style {
         /** 
--- src/com/vector/entourage/MainActivity.java
+++ src/com/vector/entourage/MainActivity.java
@@ -1,15 +1,73 @@
 package com.vector.entourage;
 
 import android.os.Bundle;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView numericE = (TextView) findViewById(R.id.numericE); 
+        TextView N = (TextView) findViewById(R.id.N);
+        TextView T = (TextView) findViewById(R.id.T);
+       // TextView O = (TextView) findViewById(R.id.O);
+        TextView U = (TextView) findViewById(R.id.U);
+        TextView r = (TextView) findViewById(R.id.R);
+        TextView A = (TextView) findViewById(R.id.A);
+        TextView G = (TextView) findViewById(R.id.G);
+        TextView E = (TextView) findViewById(R.id.E);
+        
+        ObjectAnimator neAnimation = ObjectAnimator.ofFloat(numericE, View.ALPHA, 0, 1); 
+        ObjectAnimator nAnimation = ObjectAnimator.ofFloat(N, View.ALPHA, 0, 1); 
+        ObjectAnimator tAnimation = ObjectAnimator.ofFloat(T, View.ALPHA, 0, 1); 
+     // ObjectAnimator oAnimation = ObjectAnimator.ofFloat(O, View.ALPHA, 1); 
+        ObjectAnimator uAnimation = ObjectAnimator.ofFloat(U, View.ALPHA, 0, 1); 
+        ObjectAnimator rAnimation = ObjectAnimator.ofFloat(r, View.ALPHA, 0, 1); 
+        ObjectAnimator aAnimation = ObjectAnimator.ofFloat(A, View.ALPHA, 0, 1); 
+        ObjectAnimator gAnimation = ObjectAnimator.ofFloat(G, View.ALPHA, 0, 1); 
+        ObjectAnimator eAnimation = ObjectAnimator.ofFloat(E, View.ALPHA, 0, 1);
+        
+        AnimatorSet titleSet = new AnimatorSet(); 
+        
+        //titleSet.play(neAnimation).before(nAnimation).before(tAnimation).before(uAnimation).before(rAnimation).before(aAnimation).before(gAnimation).before(eAnimation); 
+        
+        
+       // Animator[] animations = new Array(neAnimation, nAnimation, tAnimation, uAnimation, rAnimation, aAnimation, gAnimation, eAnimation); 
+        
+        titleSet.playSequentially(neAnimation, nAnimation, tAnimation, uAnimation, rAnimation, aAnimation, gAnimation, eAnimation); 
+         
+        
+        titleSet.setDuration(500); 
+        //property values holder (x and fade) 
+        
+        titleSet.start(); 
+        
+        
+        
+        // actually we will need to use both: 
+        // property animatin for the title, and transitions for the login/signup shitcanoni
+        
+        //Add code for custom animation that drops to the top of the screen and fades the view in 
+        
+        //layout transition may not be the way to go for this, as we really just want to 
+        //animate the exisiting layout, we are not ammending it
+//        LayoutTransition trans = new LayoutTransition()	; 
+//       trans.enableTransitionType(LayoutTransition.CHANGING);
+//        viewGroup = (ViewGroup) findViewById(R.id.container);
+//        viewGroup.setLayoutTransition(trans);
+        
+       // TextView e = (TextView) findViewById(R.id.numericE); 
+       // e.animate() pass the animator object to this object 
+        //viewGroup.addView(e);  
+        
     }
 
 
