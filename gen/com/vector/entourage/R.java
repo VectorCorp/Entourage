b409ef0a29ecaefc5a5ec74cfd7e4a7853fccb86
damion6891 <damion6891@yahoo.com>
2014-05-12 22:38:15 -0400

    Added drawable images for all screen sizes

--- gen/com/vector/entourage/R.java
+++ gen/com/vector/entourage/R.java
@@ -21,7 +21,7 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ent_logo_phone=0x7f020000;
+        public static final int ent_logo=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int o_icon=0x7f020002;
     }
