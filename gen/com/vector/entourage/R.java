3dcb4bcfd8ba3e6963f49e8ebcd7c9a5bdf88a02
KeonW <Keontwilliams@gmail.com>
2014-05-14 23:06:20 -0400

    Animation Revisions

--- gen/com/vector/entourage/R.java
+++ gen/com/vector/entourage/R.java
@@ -44,7 +44,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int splash_screen=0x7f030001;
+        public static final int login_scene1=0x7f030001;
+        public static final int splash_screen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
